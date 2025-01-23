ea61dc80b27c29387d85c9a222d57daed7243aaa
Brandon Wetzel <mauzybwy@gmail.com>
2025-01-22 20:28:45 -0500

    spinny

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,20 +6,7 @@ export default {
 			sans: ['"Comic Sans MS"', "sans-serif"],
 		},
 		extend: {
-			animation: {
-				marquee: "marquee 25s linear infinite",
-				marquee2: "marquee2 25s linear infinite",
-			},
-			keyframes: {
-				marquee: {
-					"0%": { transform: "translateX(0%)" },
-					"100%": { transform: "translateX(-100%)" },
-				},
-				marquee2: {
-					"0%": { transform: "translateX(100%)" },
-					"100%": { transform: "translateX(0%)" },
-				},
-			},
+
 		},
 	},
 	plugins: [],

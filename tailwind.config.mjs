1774cdf3edeca4ede6317398da87d3991c60d1bb
Brandon Wetzel <mauzybwy@gmail.com>
2025-01-22 19:55:34 -0500

    lol

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['"Comic Sans MS"', "sans-serif"],
+		},
+		extend: {
+			animation: {
+				marquee: "marquee 25s linear infinite",
+				marquee2: "marquee2 25s linear infinite",
+			},
+			keyframes: {
+				marquee: {
+					"0%": { transform: "translateX(0%)" },
+					"100%": { transform: "translateX(-100%)" },
+				},
+				marquee2: {
+					"0%": { transform: "translateX(100%)" },
+					"100%": { transform: "translateX(0%)" },
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
